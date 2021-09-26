f9de35a2454c331076b0a37daab035a11d5c2039
Rio Bahtiar <akhibahtiar@gmail.com>
2021-09-26 20:39:12 +0700

    Initial commit from editor

---
+++ wp-og-image.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WP OG Image
+ * Version: 0.0.1
+ * Requires at least: 4.9
+ * Requires PHP: 7.2.0
+ * Plugin URI: https://rio.my.id/
+ * Description: Open Graph Image Enhancer for Twitter, Facebook, Slack, etc
+ * Author: Rio B
+ * Author URI: https://rio.my.id/a/wp-og-image/
+ * Network: false
+ * Text Domain: wp-og-image
+ * Domain Path: /assets/languages
+ */
+
+/**
+ * @author    Rio B
+ * @copyright Rio B, 2021, All Rights Reserved
+ * This code is released under the GPL licence version 3 or later, available here
+ * https://www.gnu.org/licenses/gpl.txt
+ */
+
