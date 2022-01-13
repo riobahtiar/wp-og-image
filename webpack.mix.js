503609ca5a24f08fde087b9585c82624cca16821
Rio Bahtiar <akhibahtiar@gmail.com>
2022-01-14 05:04:50 +0700

    github action wpcs

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.js('assets/src/app.js', 'assets/dist');
+mix.js('assets/src/app.js', 'assets/dist').autoload({
+    jquery: ['$', 'window.jQuery']
+});
+
 mix.sass('assets/src/app.scss', 'assets/dist');
